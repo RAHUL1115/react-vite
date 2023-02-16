ce757ed777a7e0f070624bc22e7d8b266dd243da
Rahul Gupta <rahulgupta.ra.com@gmail.com>
2023-02-16 10:30:31 +0530

    init

---
+++ src/layout/Layout1.jsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+function Layout1(props) {
+    return ( <div>
+        <Navbar></Navbar>
+        {props.children}
+    </div> );
+}
+
+export default Layout1;
---
+++ src/pages/Root.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Button, Tooltip, Snackbar } from "@mui/material";
+import Layout1 from "../layout/Layout1";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("mounted")
+    return function() {
+      console.log("unmounted")
+    }
+  });
+
+  function increament() {
+    if(count == 100)  {
+      return
+    }
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    if(count == 0)  {
+      return
+    }
+    setCount(count - 1);
+  }
+
+  return (
+    <Layout1>
+      <div className="container mx-auto">
+        <div className="flex items-center gap-5 justify-center">
+          <Tooltip title="decrement" arrow>
+            <Button variant="outlined" onClick={decrement}>
+              <span className="text-2xl">-</span>
+            </Button>
+          </Tooltip>
+          <p>{count}</p>
+          <Tooltip title="Increment" arrow>
+            <Button variant="outlined" onClick={increament}>
+              <span className="text-2xl">+</span>
+            </Button>
+          </Tooltip>
+        </div>
+      </div>
+    </Layout1>
+  );
+}
+
+export default App;
